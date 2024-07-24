5b412be33fd15f93bdd538950ba4de09fa7e2446
Vladimir Mitrev <orionsbelt1017@gmail.com>
2024-07-24 14:40:36 +0300

    Add carousel to homepage

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,12 +1,17 @@
+import { useEffect, useState } from 'react';
+
 import withAuth from '../../HOC/withAuth';
 import * as crystalService from '../../services/crystalService';
-import { useEffect, useState } from 'react';
 import LatestsCrystalsItem from './latestCrystals/latestCrystalsItem';
 
+import OwlCarousel from 'react-owl-carousel';
+import 'owl.carousel/dist/assets/owl.carousel.min.css';
+import 'owl.carousel/dist/assets/owl.theme.default.min.css';
+
 const Home = ({ 
-    email,
-     _id
-    }) => {
+  email,
+  name,
+  }) => {
   const [latestCrystals, setLatestCrystals] = useState([]);
 
   useEffect(() => {
@@ -14,27 +19,43 @@ const Home = ({
       setLatestCrystals(result);
     });
   }, []);
-  console.log(latestCrystals);
+  // console.log(latestCrystals);
 
   return (
-    <div className="container-xxl py-5">
-      <div className="container">
-        <div className="text-center wow fadeInUp" data-wow-delay="0.1s">
-        <h6 className="section-title h3 bg-white text-center text-primary px-3 my-3">
-            Latest Crystals
-          </h6>
-          {/* <h1 className="mb-5">Latest Crystals</h1> */}
-        </div>
-        <div className="row g-4 justify-content-center">
-          {latestCrystals.map((crystal) => (
-            <LatestsCrystalsItem key={crystal._id} {...crystal} />
-          ))}
-          {/* <!-- Display paragraph: If there is no crystals  --> */}
+    <div>
+      <div className="container-xxl py-5 wow fadeInUp" data-wow-delay="0.1s">
+        <div className="container">
+          <div className="text-center">
+            <h1 className="mb-5 text-dark">Welcome {name ? name : 'Guest'} to Crystals Blog!!!</h1>
+            <h6 className="section-title bg-white text-center text-primary px-3">
+              Our Latest Crystals
+            </h6>
+          </div>
+          <OwlCarousel
+            className="owl-theme"
+            items={3}
+            autoplay={true}
+            smartSpeed={1000}
+            autoplaySpeed={1000}
+            autoplayTimeout={3000}
+            nav
+            dots
+            loop
+            margin={10}
+            dotsSpeed={1000}
+            navSpeed={1000}
+          >
+              {latestCrystals.map((crystal) => (
+                <LatestsCrystalsItem key={crystal._id} {...crystal} />
+              ))}
+          </OwlCarousel>
           {!latestCrystals.length && (
-            <div>
-              <p className="h2 text-danger text-center">Sorry there are no added crystals yet</p>
-              <p className="h2 text-danger text-center">:\</p>
-            </div>
+                <div>
+                  <p className="h2 text-danger text-center">
+                    Sorry there are no added crystals yet
+                  </p>
+                  <p className="h2 text-danger text-center">:\</p>
+                </div>
           )}
         </div>
       </div>
--- client/src/components/home/latestCrystals/latestCrystalsItem.jsx
+++ client/src/components/home/latestCrystals/latestCrystalsItem.jsx
@@ -1,76 +1,29 @@
-import { Link } from "react-router-dom";
-import Path from "../../../paths";
-import { pathToUrl } from "../../../utils/pathUtils";
+import { Link } from 'react-router-dom';
+import Path from '../../../paths';
+import { pathToUrl } from '../../../utils/pathUtils';
 import styles from './LatestCrystalsItem.module.css';
 import { faPalette} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-const LatestsCrystalsItem = ({
-    _id,
-    name,
-    imageUrl,
-    rarity,
-    color,
-    source,
-    healing
-}) => {
+const LatestsCrystalsItem = ({ 
+    _id, 
+    name, 
+    imageUrl, 
+    color, 
+    appearance }) => {
   return (
-    <div className="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
-            <div className="package-item">
-              <div className="overflow-hidden">
-                <img className={`img-fluid ${styles.cardImage}`} src={imageUrl} alt="Crystal Image" />
-              </div>
-              <div className="d-flex border-bottom">
-                <small className="flex-fill text-center border-end py-2">
-                  <i className="fa fa-map-marker-alt text-primary me-2"></i>
-                  {name}
-                </small>
-                <small className="flex-fill text-center border-end py-2">
-                  {/* <i className="fa fa-calendar-alt text-primary me-2"></i> {color} */}
-                  <FontAwesomeIcon icon={faPalette} /> {color}
-                </small>
-                <small className="flex-fill text-center py-2">
-                  <i className="fa fa-user text-primary me-2"></i>{rarity}
-                </small>
-              </div>
-              <div className="text-center p-4">
-                <h3 className="mb-0">{source}</h3>
-                <div className="mb-3">
-                  <small className="fa fa-star text-primary"></small>
-                  <small className="fa fa-star text-primary"></small>
-                  <small className="fa fa-star text-primary"></small>
-                  <small className="fa fa-star text-primary"></small>
-                  <small className="fa fa-star text-primary"></small>
-                </div>
-                <p>
-                  {healing}
-                </p>
-                <div className="d-flex justify-content-center mb-2">
-                  {/* <a href="#" className="btn btn-sm btn-primary px-3 border-end" style="border-radius: 30px 0 0 30px;">Read More</a>
-                                <a href="#" className="btn btn-sm btn-primary px-3" style="border-radius: 0 30px 30px 0;">Book Now</a> */}
-                </div>
-              </div>
-            </div>
-          </div>
-    // <div
-    //   className={`col-lg-4 col-md-12 wow fadeInUp ${styles.latestCard}`}
-    //   data-wow-delay="2s"
-    // >
-    //   <div className="game">
-    //     <div className="image-wrap">
-    //       <img src={imageUrl} />
-    //     </div>
-    //     <h3>{name}</h3>
-    //     <div className="rating">
-    //       <span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
-    //     </div>
-    //     <div className="data-buttons">
-    //       <Link to={pathToUrl(Path.CrystalDetails, {crystalId: _id})} className="btn details-btn">
-    //         Details
-    //       </Link>
-    //     </div>
-    //   </div>
-    // </div>
+    <div className={`item testimonial-item bg-white text-center border p-4 ${styles.latestCard}`}>
+        <img
+        className={`bg-white rounded-circle shadow p-1 mx-auto mb-3 ${styles.latestCardImage}`}
+        src={imageUrl}
+        />
+        <h5 className="mb-0">{name}</h5>
+        <p><FontAwesomeIcon icon={faPalette} /> {color}</p>
+        <p className="mb-0">{appearance}</p>
+        <Link to={pathToUrl(Path.CrystalDetails, {crystalId: _id})} 
+            className="btn details-btn btn-primary rounded-3">Details
+        </Link>
+    </div>
   );
 };
 
