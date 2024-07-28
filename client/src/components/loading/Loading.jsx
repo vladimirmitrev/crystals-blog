49f953726b89717c1c178a6b75f3e4326e0bcbf7
Vladimir Mitrev <orionsbelt1017@gmail.com>
2024-07-28 13:41:53 +0300

    Add text to loader

--- client/src/components/loading/Loading.jsx
+++ client/src/components/loading/Loading.jsx
@@ -5,6 +5,7 @@ const Loading = () => {
     return (
         <div className={styles.loaderContainer} data-testid="loader">
             <div className={styles.loader}>
+                <h6>Loading...</h6>
                 <div className={`${styles.inner} ${styles.innerOne}`} />
                 <div className={`${styles.inner} ${styles.innerTwo}`} />
                 <div className={`${styles.inner} ${styles.innerThree}`} />
